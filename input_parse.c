a932270c57203082ca2a5a37182a740d70ea02e5
Hawmee <nomenaloicrandriann@gmail.com>
2026-05-08 13:09:19 +0300

    input_parse.c: Done

---
+++ input_parse.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   input_parse.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/08 05:59:12 by varandri          #+#    #+#             */
+/*   Updated: 2026/05/08 06:02:48 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	parse(char **argv, t_stack *a, t_flags *flags)
+{
+	char	*argv_joined;
+	char	**clear_input;
+}
