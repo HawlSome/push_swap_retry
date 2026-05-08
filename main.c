a698deac02c768a83b2da2bae14fcb9f809cff25
Hawmee <nomenaloicrandriann@gmail.com>
2026-05-08 17:18:08 +0300

    main.c: Done

---
+++ main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/08 15:11:54 by varandri          #+#    #+#             */
+/*   Updated: 2026/05/08 15:15:28 by varandri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack 	*a;
+	t_stack		*b;
+	t_moves		*moves;
+	t_flags		*flags;
+
+	a = NULL;
+	b = NULL;
+	moves = NULL;
+	flags = NULL;
+
+
+	return (0);
+}
