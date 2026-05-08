2396a34e908d9730833a937ed8249168d8801991
Hawmee <nomenaloicrandriann@gmail.com>
2026-05-08 17:17:58 +0300

    input_parse.c: Done

--- input_parse.c
+++ input_parse.c
@@ -6,14 +6,24 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 05:59:12 by varandri          #+#    #+#             */
-/*   Updated: 2026/05/08 06:02:48 by varandri         ###   ########.fr       */
+/*   Updated: 2026/05/08 16:55:07 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	parse(char **argv, t_stack *a, t_flags *flags)
+int	parse(int argc, char **argv, t_stack **a, t_flags **flags)
 {
 	char	*argv_joined;
-	char	**clear_input;
+	char	**clean_input;
+
+	if (!argv || !*argv)
+		return ;
+	argv_joined = ft_strjoin(argc - 1, argv + 1, " ");
+	if (!argv_joined)
+		return (0);
+	clean_input = ft_split(argv_joined);
+	free(argv_joined);
+	if (!clean_input)
+		return 0;
 }
