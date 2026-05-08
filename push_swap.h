3a101c91182397633a0f6f2e6b2450cdb98836f5
Hawmee <nomenaloicrandriann@gmail.com>
2026-05-08 13:09:29 +0300

    push_swap.h: Done

--- push_swap.h
+++ push_swap.h
@@ -25,5 +25,14 @@ typedef struct s_flags
 }	t_flags;
 
 size_t	size_stack(t_stack *stack);
+void	stack_add_front(t_stack **stack, t_stack *node);
+t_stack	*stack_last(t_stack *stack);
+void	stack_add_back(t_stack **stack, t_stack *node);
+
+
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(char *str);
+char	*ft_substr(char *str, unsigned int start, size_t len);
+
 
 #endif
