37b0dae4e1dd8de14d546faae9c8c149a07ce41a
Hawmee <nomenaloicrandriann@gmail.com>
2026-05-08 15:06:10 +0300

    move_push.c: Done

--- move_push.c
+++ move_push.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 21:55:54 by varandri          #+#    #+#             */
-/*   Updated: 2026/05/07 22:02:55 by varandri         ###   ########.fr       */
+/*   Updated: 2026/05/08 15:01:51 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,9 @@
 void	push(t_stack **from, t_stack **to)
 {
 	t_stack	*node;
-	
+
 	if (!from || !*from)
-		return;
+		return ;
 	node = *from;
 	*from = (*from)->next;
-	
 }
