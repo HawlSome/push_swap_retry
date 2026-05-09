ee8781f02d714592c08da7f7dc6038f9140d751b
varandri <varandri@student.42antananarivo.mg>
2026-05-09 04:31:13 +0300

    main.c: Done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 15:11:54 by varandri          #+#    #+#             */
-/*   Updated: 2026/05/08 15:15:28 by varandri         ###   ########.fr       */
+/*   Updated: 2026/05/09 04:30:44 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,21 @@
 
 int	main(int argc, char **argv)
 {
-	t_stack 	*a;
+	t_stack		*a;
 	t_stack		*b;
 	t_moves		*moves;
 	t_flags		*flags;
 
+	if (argc <= 1)
+		return (0);
 	a = NULL;
 	b = NULL;
 	moves = NULL;
 	flags = NULL;
-
-
+	if (!parse(argc, argv, &a, &flags))
+	{
+		ft_printf(2, "Error");
+		return (0);
+	}
 	return (0);
 }
