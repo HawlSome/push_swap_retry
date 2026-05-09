dc784757cb17512c7ae370116badb580ac2f0242
varandri <varandri@student.42antananarivo.mg>
2026-05-09 04:31:17 +0300

    move_push.c: Done

--- move_push.c
+++ move_push.c
@@ -6,18 +6,18 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 21:55:54 by varandri          #+#    #+#             */
-/*   Updated: 2026/05/08 15:01:51 by varandri         ###   ########.fr       */
+/*   Updated: 2026/05/09 04:09:25 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push(t_stack **from, t_stack **to)
-{
-	t_stack	*node;
+// void	push(t_stack **from, t_stack **to)
+// {
+// 	t_stack	*node;
 
-	if (!from || !*from)
-		return ;
-	node = *from;
-	*from = (*from)->next;
-}
+// 	if (!from || !*from)
+// 		return ;
+// 	node = *from;
+// 	*from = (*from)->next;
+// }
