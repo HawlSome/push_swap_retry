20feb3860999405a9f84c0611cf7d5387b68d809
varandri <varandri@student.42antananarivo.mg>
2026-05-09 04:31:21 +0300

    push_swap.h: Done

--- push_swap.h
+++ push_swap.h
@@ -24,10 +24,17 @@ typedef struct s_flags
 	struct s_flags	*next;
 }	t_flags;
 
+t_stack	*new_stack_node(int value);
 size_t	size_stack(t_stack *stack);
 void	stack_add_front(t_stack **stack, t_stack *node);
-t_stack	*stack_last(t_stack *stack);
 void	stack_add_back(t_stack **stack, t_stack *node);
+void	new_stack(t_stack **stack, int value);
+
+t_flags	*new_flag_node(char *algo, int is_adaptive);
+size_t	size_flag(t_flags *flag);
+void	flag_add_front(t_flags **flag, t_flags *node);
+void	flag_add_back(t_flags **flag, t_flags *node);
+void	new_flag(t_flags **flag, char *algo, int is_adaptive);
 
 void	*ft_calloc(size_t nmemb, size_t size);
 size_t	ft_strlen(char *str);
@@ -35,5 +42,16 @@ char	*ft_substr(char *str, unsigned int start, size_t len);
 void	free_2d(char **arr_str);
 char	*ft_strjoin(size_t argc, char **argv, char *delim);
 char	**ft_split(char *str);
+int		ft_strcmp(char *str_1, char *str_2);
+
+int		ft_atoi(char *str);
+int		is_over_underlow(char *str);
+int		is_str_int(char *str);
+int		is_str_int_present(char **arr_str);
+
+int		is_clone(char *current, char *next);
+int		is_valid_flag(char *input);
+int		verif(char **clean_input);
+int		parse(int argc, char **argv, t_stack **a, t_flags **flags);
 
 #endif
