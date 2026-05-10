6c3124a5139ae5db0304562683f932c0b42fffbe
varandri <varandri@student.42antananarivo.mg>
2026-05-10 04:51:57 +0300

    move_push.c: Done

--- move_push.c
+++ move_push.c
@@ -6,18 +6,35 @@
 /*   By: varandri <varandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 21:55:54 by varandri          #+#    #+#             */
-/*   Updated: 2026/05/09 04:09:25 by varandri         ###   ########.fr       */
+/*   Updated: 2026/05/10 04:00:11 by varandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// void	push(t_stack **from, t_stack **to)
-// {
-// 	t_stack	*node;
+static void	push(t_stack **from, t_stack **to)
+{
+	t_stack	*node;
 
-// 	if (!from || !*from)
-// 		return ;
-// 	node = *from;
-// 	*from = (*from)->next;
-// }
+	if (!from || !*from)
+		return ;
+	node = *from;
+	*from = (*from)->next;
+	// node->next  = NULL;
+	stack_add_front(to, node);
+}
+
+
+void	pa(t_stack **a, t_stack **b, t_moves **moves, int save)
+{
+	push(a, b);
+	if (save)
+		new_move(moves, "pa");
+}
+
+void	pb(t_stack **a, t_stack **b, t_moves **moves, int save)
+{
+	push(b, a);
+	if (save)
+		new_move(moves, "pb");	
+}
